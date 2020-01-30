059f135ef8e6a3b27ce223610133a2aa98b00031
kcqnly <1350925894@qq.com>
2020-01-30 16:27:06 +0800

    【feat】edition-1

---
+++ .vscode/compile.bat
@@ -0,0 +1,33 @@
+@echo off
+color 87
+set KEIL_BIN_FOLDER=D:\Program Files\Keil5\C51\BIN
+cls
+echo compiling....
+echo %1
+echo %2
+echo %3
+cd /D %2
+for %%i in (*.c) do ( 
+    echo %%i
+    call "%KEIL_BIN_FOLDER%\C51.EXE" %%i 
+)
+for %%i in (*.LST) do ( move %%i "%1\output" > NUL )
+for %%i in (*.OBJ) do ( move %%i "%1\output" > NUL )
+call "%KEIL_BIN_FOLDER%\A51.EXE" %3\STARTUP.A51 >NUL
+
+move "%3\STARTUP.OBJ" "%1\output" > NUL
+move "%3\STARTUP.LST" "%1\output" > NUL
+cd %1/output
+@setlocal enableextensions enabledelayedexpansion
+set result=%3\C51S.LIB
+for %%i in (*.OBJ) do (
+    set result=!result!,%%i
+)
+echo %result%
+
+call "%KEIL_BIN_FOLDER%\BL51.EXE" %result% TO %4 
+call "%KEIL_BIN_FOLDER%\OH51.EXE" "%1\output\%4" 
+
+
+echo Compiled successfully: output\%4.hex
+exit 0
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # VSCode-C51
 VSCode Keil C51工程模板
+![VSCode-C51](https://ly-object-1259106193.cos.ap-chengdu.myqcloud.com/github/C51.png)
+
+# 文件夹说明
+- src\ 存放代码文件
+- output\ 编译过程生成的文件
+- environment\ 51启动文件和LIB静态连编文件
+
+# 修改说明
+## 1、修改tasks.json
+```json
+  "args": [
+    "\"${workspaceFolder}\"",
+    "\"${workspaceFolder}\\src\"",
+    "\"${workspaceFolder}\\environment\"",
+    "main" 
+],
+```
+主要修改第四个参数"main"，这个相当于Keil中的工程名
+
+## 2、修改compile.bat
+```bash
+@echo off
+color 87
+set KEIL_BIN_FOLDER=D:\Program Files\Keil5\C51\BIN
+```
+将KEIL_BIN_FOLDER修改为你的C51\BIN的地址
+
+# 注意事项
+所有的代码文件必须放到src根目录下面，
+编译时，点击终端-运行生成任务
+
+# 参考项目
+https://github.com/kenvix/VSCode-Keil-C51-Environment
