c541657d1319c504b82f769802bc7cd958a4d20c
kcqnly <kcqnly@qq.com>
2020-09-29 16:21:22 +0800

    更改项目结构

--- .vscode/compile.bat
+++ .vscode/compile.bat
@@ -4,29 +4,36 @@ cls
 echo author:kcqnly
 echo compiling....
 
+if not exist %~1\build ( 
+    mkdir %~1\build 
+    mkdir %~1\build\Listing 
+    mkdir %~1\build\Obj 
+)
+
 cd %~2
 for %%i in (*.c) do ( 
     echo %%i
-    call "%KEIL_BIN_FOLDER%\C51.EXE" %%i >> %~1/.vscode/compile.log
+    call "%KEIL_BIN_FOLDER%\C51.EXE" %%i >> %~1\build\compile.log
 )
-for %%i in (*.LST) do ( move %%i "%~1\output" > NUL )
-for %%i in (*.OBJ) do ( move %%i "%~1\output" > NUL )
+for %%i in (*.LST) do ( move %%i %~1\build > NUL )
+for %%i in (*.OBJ) do ( move %%i %~1\build > NUL )
 call "%KEIL_BIN_FOLDER%\A51.EXE" "%~3\STARTUP.A51" >NUL
 
 cd %~3
-move STARTUP.OBJ "%~1\output" > NUL
-move STARTUP.LST "%~1\output" > NUL
+move STARTUP.OBJ %~1\build > NUL
+move STARTUP.LST %~1\build > NUL
 
-if not exist %1\output\C51S.LIB ( copy "%~3\C51S.LIB" "%~1\output" > NUL)
+if not exist %1\output\C51S.LIB ( copy "%~3\C51S.LIB" "%~1\build" > NUL)
 
-cd %~1/output
+cd %~1\build
 @setlocal enableextensions enabledelayedexpansion
 set result=C51S.LIB
 for %%i in (*.OBJ) do (
     set result=!result!,%%i
 )
-call "%KEIL_BIN_FOLDER%\BL51.EXE" %result% TO %4 >> %~1/.vscode/compile.log
-call "%KEIL_BIN_FOLDER%\OH51.EXE" "%~1\output\%4" >> %~1/.vscode/compile.log
-
-if exist %1\output\%4.hex ( echo Compilation completed: output\%4.hex )
+call "%KEIL_BIN_FOLDER%\BL51.EXE" %result% TO %4 >> "%~1\build\compile.log"
+call "%KEIL_BIN_FOLDER%\OH51.EXE" "%~1\build\%4" >> "%~1\build\compile.log"
+for %%i in (*.LST) do ( move %%i %~1\build\Listing > NUL )
+for %%i in (*.OBJ) do ( move %%i %~1\build\Obj > NUL )
+if exist %~1\build\%4.hex ( echo Compilation completed: build\%4.hex )
 exit 0
