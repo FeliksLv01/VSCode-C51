f3f7bd975a3d6b769be488deae0c3efa0c249145
kcqnly <kcqnly@qq.com>
2020-10-01 10:33:40 +0800

    :bug:错误提示

--- .vscode/compile.bat
+++ .vscode/compile.bat
@@ -13,17 +13,17 @@ if not exist %~1\build (
 cd %~2
 for %%i in (*.c) do ( 
     echo %%i
-    call "%KEIL_BIN_FOLDER%\C51.EXE" %%i >> %~1\build\compile.log
+    call "%KEIL_BIN_FOLDER%\C51.EXE" %%i >> %~1\build\compile.log || goto error
 )
-for %%i in (*.LST) do ( move %%i %~1\build > NUL )
-for %%i in (*.OBJ) do ( move %%i %~1\build > NUL )
-call "%KEIL_BIN_FOLDER%\A51.EXE" "%~3\STARTUP.A51" >NUL
+for %%i in (*.LST) do ( move %%i %~1\build > NUL ) || goto error
+for %%i in (*.OBJ) do ( move %%i %~1\build > NUL ) || goto error
+call "%KEIL_BIN_FOLDER%\A51.EXE" "%~3\STARTUP.A51" >NUL || goto error
 
 cd %~3
-move STARTUP.OBJ %~1\build > NUL
-move STARTUP.LST %~1\build > NUL
+move STARTUP.OBJ %~1\build > NUL || goto error
+move STARTUP.LST %~1\build > NUL || goto error
 
-if not exist %1\output\C51S.LIB ( copy "%~3\C51S.LIB" "%~1\build" > NUL)
+if not exist %1\output\C51S.LIB ( copy "%~3\C51S.LIB" "%~1\build" > NUL) || goto error
 
 cd %~1\build
 @setlocal enableextensions enabledelayedexpansion
@@ -31,9 +31,13 @@ set result=C51S.LIB
 for %%i in (*.OBJ) do (
     set result=!result!,%%i
 )
-call "%KEIL_BIN_FOLDER%\BL51.EXE" %result% TO %4 >> "%~1\build\compile.log"
-call "%KEIL_BIN_FOLDER%\OH51.EXE" "%~1\build\%4" >> "%~1\build\compile.log"
-for %%i in (*.LST) do ( move %%i %~1\build\Listing > NUL )
-for %%i in (*.OBJ) do ( move %%i %~1\build\Obj > NUL )
+call "%KEIL_BIN_FOLDER%\BL51.EXE" %result% TO %4 >> "%~1\build\compile.log" || goto error
+call "%KEIL_BIN_FOLDER%\OH51.EXE" "%~1\build\%4" >> "%~1\build\compile.log" || goto error
+for %%i in (*.LST) do ( move %%i %~1\build\Listing > NUL || goto error )
+for %%i in (*.OBJ) do ( move %%i %~1\build\Obj > NUL || goto error)
 if exist %~1\build\%4.hex ( echo Compilation completed: build\%4.hex )
 exit 0
+
+:error
+echo Error!!!: error levele %errorlevel%
+exit %errorlevel%
